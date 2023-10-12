762710ffe2507462f6a1e63b4894b2210c54d734
MichaelOdera <michogelira@gmail.com>
2023-10-12 23:07:33 +0300

    rectify the statements attached to the options for converting from airline miles to cash and vice versa to be user friendly

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -5,19 +5,27 @@ public class RewardsConverter {
         var scanner = new Scanner(System.in);
         System.out.println("Welcome to the Credit Card Rewards Converter!");
         System.out.println("Enter 1 or 2 to choose the conversion you would like to have");
-        System.out.println("1. Convert Cash to Miles");
-        System.out.println("2. Convert Miles to Cash");
+        System.out.println("1. Convert Cash to Airline Miles");
+        System.out.println("2. Convert Airline Miles to Cash");
 
         var choice_value = scanner.nextLine();
         double choiceValue;
         try {
             choiceValue = Integer.parseInt(choice_value);
+
+            //handle the edge case for if the choice picked is not 1 or two
+            if(choiceValue < 1 || choiceValue > 2){
+                System.out.println("Please enter value 1 or 2, exiting");
+                return;
+            }
         } catch (NumberFormatException exception) {
+            // handle the edge case for if the value entered is not an integer or is not convertible to int 1 or 2
             System.out.println("Please enter value 1 or 2, exiting");
             return;
         }
 
         if(choiceValue == 1){
+            //Main code block for converting cash to airline miles
             System.out.println("Please enter a cash value to convert to airline miles: ");
             var input_value = scanner.nextLine();
             double cashValue;
@@ -30,7 +38,8 @@ public class RewardsConverter {
             System.out.println("converting $" + input_value + " to miles");
             var rewardsValue = new RewardValue(cashValue);
             System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
-        }else {
+        }else{
+            // Main code block for converting airline miles to cash
             System.out.println("Please enter an airline miles value to convert to cash: ");
             var miles_value = scanner.nextLine();
             double cashValue;
